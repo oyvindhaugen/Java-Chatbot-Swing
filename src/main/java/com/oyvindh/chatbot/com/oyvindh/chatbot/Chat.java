31451dda936ce7512e52241ea418fa82a58547f3
oyvindhaugen <oyvindet@gmail.com>
2022-01-02 19:03:23 +0100

    Finished and working. Might add some other phrases.

--- src/main/java/com/oyvindh/chatbot/com/oyvindh/chatbot/Chat.java
+++ src/main/java/com/oyvindh/chatbot/com/oyvindh/chatbot/Chat.java
@@ -24,45 +24,46 @@ class Chat extends JFrame implements ActionListener {
       {"programs", "languages", "language", "is"},
       {"teaches", "attends"}
   };
-  private static final String[] VERBS_FOR_SENTENCES = new String[]{"what do you ", "how do you ", "what ",
-      "who "};
+  private static final String[] VERBS_FOR_SENTENCES = new String[]{"what do you", "how do you",
+      "what",
+      "who"};
   private static final String[] ANSWER_CONTENT_STATUS = new String[]{/*[0]*/
-      "Hello! My name is IM Haugaland Bot. " + "\n"
+       "\n" + "Hello! My name is IM Haugaland Bot. " + "\n"
           + "The topics I can answer are questions about IM at Haugaland Upper Secondary School."
           + "\n",
-      /*[1]*/"You learn about programming, security, hardware and software, and media etc." + "\n",
+      /*[1]*/"\n" + "You learn about programming, security, hardware and software, and media etc." + "\n",
       /*[2]*/
-      "Here at IM we have fun, learning and having a great class environment that is friendly to all. "
+      "\n" + "Here at IM we have fun, learning and having a great class environment that is friendly to all. "
           + "\n" + "We start our day at 8 am and end at 3 pm with a never ending smile!"
           + "\n",
       /*[3]*/
-      "At IM you get a friendly environment, friends to have for life and a good way into a futuristic work field. "
+      "\n" + "At IM you get a friendly environment, friends to have for life and a good way into a futuristic work field. "
           + "\n",
-      "At IM there is lots of group work to simulate the normal work life in this field. "
+      "\n" + "At IM there is lots of group work to simulate the normal work life in this field. "
           + "\n"
           + "There are some single person tasks, but it's mainly centered around group work. "
           + "\n",
-      "Every subject starts with a small theoretical part where you learn what the reason you are learning this and the basics of that thing. "
+      "\n" + "Every subject starts with a small theoretical part where you learn what the reason you are learning this and the basics of that thing. "
           + "\n" + "Then you get assigned a few tasks to let you get a little used to it. "
           + "\n"
           + "At the end you get assigned one bigger task that will take some more time and some more work. This way it will be easier to learn. "
           + "\n",
-      "At IM you learn about the Adobe Creative Cloud package of programs including Photoshop, Premiere Pro and Illustrator to name a few. "
+      "\n" + "At IM you learn about the Adobe Creative Cloud package of programs including Photoshop, Premiere Pro and Illustrator to name a few. "
           + "\n"
           + "You also learn programs such as Visual Studio Code, which is an Integrated Developer Environment for programming. "
           + "\n",
-      "You can learn basically any language you want to learn. Be it C++, Python or even Assembly, all of these are possibilities."
+      "\n" + "You can learn basically any language you want to learn. Be it C++, Python or even Assembly, all of these are possibilities."
           + "\n",
-      "You are expected to be a good student and to finish your work, but " + "\n"
+      "\n" + "You are expected to be a good student and to finish your work, but " + "\n"
           + "as important as that is it's even more important to be a good classmate and a good group worker."
           + "\n",
-      "At IM VG1, we have 3 teachers: Åsmund Kringlebotten, Kjartan Haavik and Odd Arne Hemmingstad. "
+      "\n" + "At IM VG1, we have 3 teachers: Åsmund Kringlebotten, Kjartan Haavik and Odd Arne Hemmingstad. "
           + "\n"
           + "At IM VG2, we have Julie Holst Pedersen, who often has help from the teamleader: Rune Karlsen. "
           + "\n"
           + "All of these are amazing teachers, who have a lot of passion for their work."
           + "\n",
-      "Everyone can attend at IM Haugaland, even you!" + "\n"
+      "\n" + "Everyone can attend at IM Haugaland, even you!" + "\n"
           + "This is an amazing work field to get into and one that will be very prevalent in the future."
           + "\n"
   };
@@ -106,7 +107,7 @@ class Chat extends JFrame implements ActionListener {
     String string = e.getActionCommand();
     String inputContent = input.getText();
     String[] endingsForSentences = {};
-    String answerContentError = "I cannot answer that, please try with a different question.";
+    String answerContentError = "\n" + "I cannot answer that, please try with a different question.";
     if (string.equals("Send")) {
       sendMessage(inputContent, answerContentError);
     }
@@ -127,6 +128,7 @@ class Chat extends JFrame implements ActionListener {
         input.setText(null);
       } else {
         answerContent.append(answerContentError + "\n");
+        input.setText(null);
       }
     } else if (inputContent.toLowerCase().startsWith(VERBS_FOR_SENTENCES[1])) {
       // how do you
@@ -134,24 +136,24 @@ class Chat extends JFrame implements ActionListener {
       if (restInput.toLowerCase().startsWith(NOUNS_FOR_SENTENCES[1][0])) {
         answerContent.append(ANSWER_CONTENT_STATUS[4]);
         input.setText(null);
-      } else if (inputContent.toLowerCase().startsWith(NOUNS_FOR_SENTENCES[1][1])) {
+      } else if (restInput.toLowerCase().startsWith(NOUNS_FOR_SENTENCES[1][1])) {
         answerContent.append(ANSWER_CONTENT_STATUS[5]);
         input.setText(null);
       }
     } else if (inputContent.toLowerCase().startsWith(VERBS_FOR_SENTENCES[2])) {
       //what
       String restInput = inputContent.substring(VERBS_FOR_SENTENCES[2].length()).trim();
-      if (inputContent.toLowerCase().startsWith(NOUNS_FOR_SENTENCES[2][0])) {
+      if (restInput.toLowerCase().startsWith(NOUNS_FOR_SENTENCES[2][0])) {
         answerContent.append(ANSWER_CONTENT_STATUS[6]);
         input.setText(null);
-      } else if (inputContent.toLowerCase().startsWith(NOUNS_FOR_SENTENCES[2][1])) {
+      } else if (restInput.toLowerCase().startsWith(NOUNS_FOR_SENTENCES[2][1])) {
         answerContent.append(ANSWER_CONTENT_STATUS[7]);
         input.setText(null);
-      } else if (inputContent.toLowerCase().startsWith(NOUNS_FOR_SENTENCES[2][2])) {
-        answerContent.append(ANSWER_CONTENT_STATUS[8]);
+      } else if (restInput.toLowerCase().startsWith(NOUNS_FOR_SENTENCES[2][2])) {
+        answerContent.append(ANSWER_CONTENT_STATUS[7]);
         input.setText(null);
-      } else if (inputContent.toLowerCase().startsWith(NOUNS_FOR_SENTENCES[2][3])) {
-        answerContent.append(ANSWER_CONTENT_STATUS[9]);
+      } else if (restInput.toLowerCase().startsWith(NOUNS_FOR_SENTENCES[2][3])) {
+        answerContent.append(ANSWER_CONTENT_STATUS[8]);
         input.setText(null);
       } else {
         answerContent.append(answerContentError + "\n");
@@ -159,17 +161,19 @@ class Chat extends JFrame implements ActionListener {
     } else if (inputContent.toLowerCase().startsWith(VERBS_FOR_SENTENCES[3])) {
       // who
       String restInput = inputContent.substring(VERBS_FOR_SENTENCES[3].length()).trim();
-      if (inputContent.toLowerCase().startsWith(NOUNS_FOR_SENTENCES[3][0])) {
-        answerContent.append(ANSWER_CONTENT_STATUS[10]);
+      if (restInput.toLowerCase().startsWith(NOUNS_FOR_SENTENCES[3][0])) {
+        answerContent.append(ANSWER_CONTENT_STATUS[9]);
         input.setText(null);
-      } else if (inputContent.toLowerCase().startsWith(NOUNS_FOR_SENTENCES[3][1])) {
-        answerContent.append(ANSWER_CONTENT_STATUS[11]);
+      } else if (restInput.toLowerCase().startsWith(NOUNS_FOR_SENTENCES[3][1])) {
+        answerContent.append(ANSWER_CONTENT_STATUS[10]);
         input.setText(null);
       } else {
         answerContent.append(answerContentError + "\n");
+        input.setText(null);
       }
     } else {
       answerContent.append(answerContentError + "\n");
+      input.setText(null);
     }
   }
 }
